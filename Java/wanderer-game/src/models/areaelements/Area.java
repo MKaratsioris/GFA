ee089387735080b66abb1c9c464094f42206c0e0
michaliskaratsiorispartner <michalis.karatsioris@partner.bmwgroup.com>
2025-04-22 13:13:00 +0200

    added Java Foundation code and Wanderer game

---
+++ Java/wanderer-game/src/models/areaelements/Area.java
@@ -0,0 +1,64 @@
+package models;
+
+import models.areaelements.Floor;
+import models.areaelements.Tile;
+import models.areaelements.Wall;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Area {
+    private List<Tile[][]> tiles;
+
+    public Area() {
+        tiles = new ArrayList<>();
+        generateTiles();
+    }
+
+    private void generateTiles() {
+        int[][] firstTiles =
+                {
+                        {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 1, 0, 0, 0, 0, 1, 1, 1, 1},
+                        {0, 1, 1, 1, 1, 0, 1, 0, 0, 0},
+                        {0, 0, 0, 0, 1, 0, 1, 0, 0, 0},
+                        {0, 0, 1, 0, 1, 0, 1, 0, 0, 0},
+                        {0, 1, 1, 0, 1, 0, 1, 1, 0, 1},
+                        {0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+                        {1, 1, 1, 1, 1, 0, 1, 0, 1, 0},
+                        {0, 0, 0, 0, 0, 0, 1, 0, 1, 0},
+                        {0, 0, 1, 1, 1, 0, 0, 0, 1, 0}};
+
+        tiles.add(generateTileMap(firstTiles));
+
+        int[][] secondTiles =
+                {
+                        {0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                        {0, 1, 0, 0, 0, 0, 1, 1, 1, 1},
+                        {0, 1, 1, 1, 1, 0, 1, 0, 0, 0},
+                        {0, 0, 0, 0, 1, 0, 1, 0, 0, 0},
+                        {0, 0, 1, 0, 1, 0, 1, 0, 0, 0},
+                        {0, 1, 1, 0, 1, 0, 1, 1, 0, 1},
+                        {0, 0, 0, 0, 0, 0, 1, 0, 0, 0},
+                        {1, 1, 1, 1, 1, 0, 1, 0, 1, 0},
+                        {0, 0, 0, 0, 0, 0, 1, 0, 1, 0},
+                        {0, 0, 1, 1, 1, 0, 0, 0, 1, 0}};
+        tiles.add(generateTileMap(secondTiles));
+    }
+
+    @NotNull
+    private static Tile[][] generateTileMap(int[][] firstTiles) {
+        Tile[][] firstTileMap = new Tile[firstTiles.length][firstTiles.length];
+        for (int i = 0; i < firstTiles.length; i++) {
+            for (int j = 0; j < firstTiles[i].length; j++) {
+                firstTileMap[i][j] = firstTiles[i][j] == 0 ? new Floor() : new Wall();
+            }
+        }
+        return firstTileMap;
+    }
+
+    public Tile[][] getTiles(int index) {
+        return tiles.get(index);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Projects
+# Green Fox Academy
+
